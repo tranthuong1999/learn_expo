1af96b2e258f68d8ac35b18a434e039bde8f5b19
MartinTran <macpro2015@MartinTran.local>
2023-10-15 11:12:55 +0700

    start expo

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {View, Switch, StyleSheet} from 'react-native';
+import Welcome from './sreens/Welcome';
 
-export default function App() {
+const App = () => {
+
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Welcome/>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+export default App;
---
+++ sreens/Welcome.tsx
@@ -0,0 +1,12 @@
+import {Text, StyleSheet} from 'react-native';
+
+
+const Welcome = () => {
+  return (
+    <Text>
+        Welcome to vietnam
+    </Text>
+  )
+}
+
+export default Welcome
